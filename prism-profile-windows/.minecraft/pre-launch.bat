d9a907998efd54553f0c3d10fdea4f7662f3fd88
Josef de Joanelli <jmdejoanelli@gmail.com>
2024-07-11 12:46:24 +1000

    Update and deprecate all ourdated mods, update vanilla tweaks

--- prism-profile-windows/.minecraft/pre-launch.bat
+++ prism-profile-windows/.minecraft/pre-launch.bat
@@ -4,5 +4,5 @@ set inst_mc_dir=%2
 shift
 shift
 
-%inst_java% -jar "%inst_mc_dir%/bootstrap.jar" "https://raw.githubusercontent.com/josef-dijon/lunarkraft/main/bootstrap_manifest.json" "%inst_mc_dir%"
-%inst_java% -jar "%inst_mc_dir%/mod-updater.jar" "https://raw.githubusercontent.com/josef-dijon/lunarkraft/main/mod_manifest.json" "%inst_mc_dir%"
+%inst_java% -jar "%inst_mc_dir%/bootstrap.jar" "https://raw.githubusercontent.com/josef-dijon/lunarkraft/1.21/bootstrap_manifest.json" "%inst_mc_dir%"
+%inst_java% -jar "%inst_mc_dir%/mod-updater.jar" "https://raw.githubusercontent.com/josef-dijon/lunarkraft/1.21/mod_manifest.json" "%inst_mc_dir%"
